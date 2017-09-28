5c4fd9058b7b081120ddcdada3796c8f059b1d2b
Tracy Fernandez <tr.ferna@gmail.com>
2017-09-28 20:29:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+chocolateBars;
+
+function addElementToBeginningOfArray('array', 'element') {
+  return ['element', ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray('array', 'element') {
+
+}
